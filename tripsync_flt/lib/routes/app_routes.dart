a060a45bdca8d48b535e121f29f068ee94a7e688
nghiemqsang <nghiemlong1k9@gmail.com>
2025-12-24 22:18:03 +0700

    [FEATURE]: Feature checklist_screen

--- tripsync_flt/lib/routes/app_routes.dart
+++ tripsync_flt/lib/routes/app_routes.dart
@@ -5,6 +5,7 @@ import '../features/auth/presentation/screens/register_screen.dart';
 import '../features/home/presentation/screens/home_screen.dart';
 import '../features/trip/presentation/screens/create_trip_screen.dart';
 import '../features/itinerary/presentation/screens/itinerary_screen.dart';
+import '../features/checklist/presentation/screens/checklist_screen.dart';
 import '../features/trip/domain/entities/trip.dart';
 
 class AppRoutes {
@@ -15,6 +16,7 @@ class AppRoutes {
   static const home = "/home";
   static const createTrip = "/create-trip";
   static const itinerary = "/itinerary";
+  static const checklist = "/checklist";
 
   static final routes = <String, WidgetBuilder>{
     start: (_) => const StartScreen(),
@@ -25,6 +27,10 @@ class AppRoutes {
     itinerary: (context) {
       final trip = ModalRoute.of(context)!.settings.arguments as Trip;
       return TripItineraryScreen(trip: trip);
+    },
+    checklist: (context) {
+      final trip = ModalRoute.of(context)!.settings.arguments as Trip;
+      return ChecklistScreen(trip: trip);
     },
   };
 }
